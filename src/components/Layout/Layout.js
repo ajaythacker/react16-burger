4d9ba470cd1b195c1f99577b850e6d70e49d3ae3
Ajay Thacker <athacker@healthgrades.com>
2018-01-30 13:39:53 -0700

    adding bit of style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxx';
+import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
     <div>Toolbar,Sidedrawer,backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
